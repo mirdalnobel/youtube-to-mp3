4257b9a965ffd73dbde4f18b37727edb3a0562ec
Mirdal Nobel Hamun <34174584+mirdalnobel@users.noreply.github.com>
2023-12-05 03:49:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, request, jsonify
+import streamlit as st
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
 from pathlib import Path
 
-app = Flask(__name__)
-
-@app.route('/convert', methods=['POST'])
-def convert():
-    data = request.get_json()
-    youtube_url = data['youtube_url']
-
+# Fungsi untuk mengonversi video YouTube ke MP3
+def convert(youtube_url):
     # Download playlist
     playlist = YouTube(youtube_url)
     for video in playlist.video_urls:
@@ -24,7 +19,16 @@ def convert():
         clip.audio.write_audiofile(mp3_path)
         clip.close()
 
-    return jsonify({"message": "Conversion complete!"})
+    return mp3_path
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Streamlit UI
+st.title("YouTube to MP3 Converter")
+
+# Mendapatkan URL YouTube dari pengguna
+youtube_url = st.text_input("Masukkan URL YouTube:")
+
+if st.button("Konversi"):
+    if youtube_url:
+        st.info("Sedang mengonversi... Ini mungkin memakan waktu beberapa saat.")
+        mp3_path = convert(youtube_url)
+        st.success(f"Konversi selesai! File MP3 tersimpan di {mp3_path}")
